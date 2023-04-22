266d53f350862e1a15edb99288ff3a55638d6441
JeysonBautista <111783892+JeysonBautista@users.noreply.github.com>
2023-04-21 19:01:20 -0500

    Update 04_EfrainBautista_dbSalesClothes.sql
    .

--- 04_EfrainBautista_dbSalesClothes.sql
+++ 04_EfrainBautista_dbSalesClothes.sql
@@ -1,15 +1,23 @@
-DROP DATABASE IF EXISTS db_SalesClothes;
+/* Poner en uso base de datos MASTER */
+USE master
+GO
 
-CREATE DATABASE db_SalesClothes;
+/* Si la base de datos ya existe la eliminamos */
+DROP DATABASE IF EXISTS db_SalesClothes
+GO
 
-USE db_SalesClothes;
+/* Crear base de datos Sales Clothes */
+CREATE DATABASE db_SalesClothes
+GO
 
-USE master;
+/* Poner en uso la base de datos */
+USE db_SalesClothes
+GO
 
-
+/* Crear tabla client */
 CREATE TABLE client
 (
-	id int,
+	id int identity(1,1),
 	type_document char(3),
 	number_document char(15),
 	names varchar(60),
@@ -19,111 +27,89 @@ CREATE TABLE client
 	birthdate date,
 	active bit
 	CONSTRAINT client_pk PRIMARY KEY (id)
-);
+)
+GO
 
+/* Crear tabla seller */
+CREATE TABLE  seller
+(
+	id int identity(1,1),
+	type_document char(3),
+	numer_document char(15),
+	names varchar(60),
+	last_name varchar(90),
+	salary decimal(8,2),
+	cell_phone char(9),
+	email varchar(80),
+	active bit
+	CONSTRAINT seller_pk PRIMARY KEY (id)
+)
+GO
 
-EXEC sp_columns @table_name = 'client';
+/* Crear tabla clothes */
+CREATE TABLE clothes
+(
+	id int identity(1,1),
+	descriptions varchar(60),
+	brand varchar(60),
+	amount int,
+	size varchar(10),
+	price decimal(8,2),
+	active bit
+	CONSTRAINT clothes_pk PRIMARY KEY (id)
+)
+GO
 
+/* Crear tabla sale */
+CREATE TABLE sale
+(
+	id int identity(1,1),
+	date_time datetime,
+	seller_id int,
+	client_id int,
+	active bit
+	CONSTRAINT sale_pk PRIMARY KEY (id)
+)
+GO
 
-SELECT * FROM INFORMATION_SCHEMA.TABLES;
+/* Crear tabla sale_detail */
+CREATE TABLE sale_detail
+(
+	id int identity(1,1),
+	sale_id int,
+	clothes_id int,
+	amount int
+)
+GO
 
-DROP TABLE client;
-
--- Table: sale
-CREATE TABLE sale (
-    id int  NOT NULL,
-    date_time datetime  NOT NULL,
-    client_id int  NOT NULL,
-    seller_id int  NOT NULL,
-    active bit  NOT NULL,
-    CONSTRAINT sale_pk PRIMARY KEY  (id)
-);
-
+/* Relacionar tabla sale con tabla client */
 ALTER TABLE sale
-	ADD CONSTRAINT sale_client 
-	FOREIGN KEY (client_id)
+	ADD CONSTRAINT sale_client FOREIGN KEY (client_id)
 	REFERENCES client (id)
 	ON UPDATE CASCADE 
-      ON DELETE CASCADE
+	ON DELETE CASCADE
 GO
 
-SELECT 
-    fk.name [Constraint],
-    OBJECT_NAME(fk.parent_object_id) [Tabla],
-    COL_NAME(fc.parent_object_id,fc.parent_column_id) [Columna FK],
-    OBJECT_NAME (fk.referenced_object_id) AS [Tabla base],
-    COL_NAME(fc.referenced_object_id, fc.referenced_column_id) AS [Columna PK]
-FROM 
-    sys.foreign_keys fk
-    INNER JOIN sys.foreign_key_columns fc ON (fk.OBJECT_ID = fc.constraint_object_id)
-GO
-
+/* Relacionar tabla sale con seller */
 ALTER TABLE sale
-	DROP CONSTRAINT sale_client
+	ADD CONSTRAINT sale_seller FOREIGN KEY (seller_id)
+	REFERENCES seller (id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
 GO
 
--- Table: clothes
-CREATE TABLE clothes (
-    id int  NOT NULL,
-    description varchar(60)  NOT NULL,
-    brand varchar(60)  NOT NULL,
-    amount int  NOT NULL,
-    size varchar(10)  NOT NULL,
-    price decimal(8,2)  NOT NULL,
-    active bit  NOT NULL,
-    CONSTRAINT clothes_pk PRIMARY KEY  (id)
-);
-
--- Table: sale_detail
-CREATE TABLE sale_detail (
-    id int  NOT NULL,
-    sale_id int  NOT NULL,
-    clothes_id int  NOT NULL,
-    amount int  NOT NULL,
-    CONSTRAINT sale_detail_pk PRIMARY KEY  (id)
-);
-
--- Table: seller
-CREATE TABLE seller (
-    id int  NOT NULL,
-    type_document char(11)  NOT NULL,
-    number_document varchar(15)  NOT NULL,
-    names varchar(60)  NOT NULL,
-    last_name varchar(90)  NOT NULL,
-    salary decimal(8,2)  NOT NULL,
-    cell_phone char(9)  NOT NULL,
-    email varchar(80)  NOT NULL,
-    activo bit  NOT NULL,
-    CONSTRAINT seller_pk PRIMARY KEY  (id)
-);
-
--- Reference: sale_detail_clothes (table: sale_detail)
-ALTER TABLE sale_detail ADD CONSTRAINT sale_detail_clothes
-    FOREIGN KEY (clothes_id)
-    REFERENCES clothes (id);
-
-
-	-- Reference: sale_detail_sale (table: sale_detail)
-ALTER TABLE sale_detail ADD CONSTRAINT sale_detail_sale
-    FOREIGN KEY (sale_id)
-    REFERENCES sale (id);
-
-	-- Reference: sale_seller (table: sale)
-ALTER TABLE sale ADD CONSTRAINT sale_seller
-    FOREIGN KEY (seller_id)
-    REFERENCES seller (id);
-
-
-	SELECT 
-    fk.name [Constraint],
-    OBJECT_NAME(fk.parent_object_id) [Tabla],
-    COL_NAME(fc.parent_object_id,fc.parent_column_id) [Columna FK],
-    OBJECT_NAME (fk.referenced_object_id) AS [Tabla base],
-    COL_NAME(fc.referenced_object_id, fc.referenced_column_id) AS [Columna PK]
-FROM 
-    sys.foreign_keys fk
-    INNER JOIN sys.foreign_key_columns fc ON (fk.OBJECT_ID = fc.constraint_object_id)
+/* Relacionar tabla sale_detail con clothes */
+ALTER TABLE sale_detail
+	ADD CONSTRAINT sale_detail_clothes FOREIGN KEY (clothes_id)
+	REFERENCES clothes (id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
 GO
 
-
--- End of file.
+/* Relacionar tabla sale con sale_detail */
+ALTER TABLE sale_detail
+	ADD CONSTRAINT sale_detail_sale FOREIGN KEY (sale_id)
+	REFERENCES sale (id)
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+GO
